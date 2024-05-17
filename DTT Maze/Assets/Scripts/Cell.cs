8f32f8064be007ce0aa77399fa673cd1d3f913d6
KGroot <kwgroot@gmail.com>
2024-05-17 13:20:57 +0200

    Restarted the project basically as they perfomance was not fixable with the old method. Now instead of looking at generation cell first it is now wall first. This prevents overlapping walls meaning less rendering. Currently walls are properly generated but there still needs to be something to detect the empty space between walls as a cell. Without cells the chosen Randomized Depth-first algorithm won't work.

--- DTT Maze/Assets/Scripts/Cell.cs
+++ DTT Maze/Assets/Scripts/Cell.cs
@@ -4,30 +4,7 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    //The first chosen algorithm to generate a maze is a recursive depth-first search found here: https://en.wikipedia.org/wiki/Maze_generation_algorithm
-    //*edit* The algorithm will be Randomized depth-first iterative implementation so a stack will be used to 
-    //As they call each 'square' in the maze a cell they will be named the same here.
-    //The reason this algorithm is chosen first is because it's both simple and decently fast.
+    public GameObject northWall, eastWall, westWall, southWall;
 
-    ///<summary>
-    ///This class holds the information pertaining a singular cell.
-    ///Including the West, East, North and South wall.
-    ///It also holds the information whether this cell is already 'visited' or included in the current maze.
-    ///Finally this class houses functions to remove it's own walls and switch itself between visited or not.
-    ///</summary>
-
-    [SerializeField]
-    private GameObject westWall, eastWall, northWall, southWall;
-
-    public bool Visited { get; private set; }
-
-    public void Visit()
-    {
-        Visited = true;
-    }
-
-    public void ClearWall(GameObject wall)
-    {
-        wall.SetActive(false);
-    }
+    public bool visited;
 }
---
+++ DTT Maze/Assets/Scripts/WallPlacer.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    ///<summary>
+    ///A new approach where walls are placed first so there won't be overlapping walls,
+    ///as performance is a huge problem when making 250x250 mazes.
+    ///Then by looking at wall positions order the space between them into a collection of cells.
+    ///Using those cells run the randomized depth first search algorithm as we can now use recursion.
+    ///</summary>
+
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] [Range(10f, 250f)] private int mazeHeight, mazeWidth;
+
+    private Transform mazeHolder;
+    private GameObject wallHolder;
+    private Vector3 startPos, currentPos;
+    private Camera mainCam;
+
+    const float WALLLENGTH = 1f;
+
+    private void Start()
+    {
+        //Grab the gameobject on which all the maze parts will be placed
+        mazeHolder = transform.GetChild(0);
+
+        //Fix the camera position
+        mainCam = Camera.main;
+        if (Mathf.Max(mazeHeight, mazeWidth) == mazeHeight)
+            mainCam.orthographicSize = mazeHeight / 1.9f;
+        else
+            mainCam.orthographicSize = mazeWidth / 3.5f;
+
+        CreateWalls();
+    }
+
+    public void CreateWalls()
+    {
+        //First we will need a starting position from where we start placing the walls
+        startPos = new Vector3((-mazeWidth / 2f) + WALLLENGTH / 2f, 0f, (-mazeHeight / 2f) + WALLLENGTH / 2f);
+        currentPos = startPos;
+
+        //Create walls along the height of the maze
+        for (int w = 0; w < mazeHeight; w++)
+        {
+            for (int h = 0; h <= mazeWidth; h++)
+            {
+                //The current posistion of where the wall is placed is found by starting at the beginning and simply placing
+                //walls of the exact size of 1 until the loop reaches the set max size for the height.
+                currentPos = new Vector3(startPos.x + (h * WALLLENGTH) - WALLLENGTH / 2f, 0f, startPos.z + (w * WALLLENGTH) - WALLLENGTH / 2f);
+                wallHolder = Instantiate(wallPrefab, currentPos, Quaternion.identity);
+                wallHolder.name = "Vertical wall: " + w + ", " + h;
+                wallHolder.transform.SetParent(mazeHolder);
+            }
+        }
+
+        //Now repeat the same for horizontal walls
+        for (int w = 0; w <= mazeHeight; w++)
+        {
+            for (int h = 0; h < mazeWidth; h++)
+            {
+                currentPos = new Vector3(startPos.x + (h * WALLLENGTH), 0f, startPos.z + (w * WALLLENGTH) - WALLLENGTH);
+                //Main difference here is the wall needed to be rotated
+                wallHolder = Instantiate(wallPrefab, currentPos, Quaternion.Euler(0, 90, 0));
+                wallHolder.name = "Horizontal wall: " + w + ", " + h;
+                wallHolder.transform.SetParent(mazeHolder);
+            }
+        }
+    }
+}
